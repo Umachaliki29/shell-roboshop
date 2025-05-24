0d4095e67f6eb051e5d1be68852906fa65b9e789
Umachaliki29 <umashankari29@gmail.com>
2025-05-24 23:22:35 +0100

    added

--- roboshop.sh
+++ roboshop.sh
@@ -2,6 +2,7 @@
 
 AMI_ID="ami-09c813fb71547fc4f"
 SG_ID="sg-07ea3d62300302f95" # replace with your SG ID
+SUBNET_ID="subnet-02dc2ea3700e04683"
 INSTANCES=("mongodb" "redis" "mysql" "rabbitmq" "catalogue" "user" "cart" "shipping" "payment" "dispatch" "frontend")
 ZONE_ID="Z06495662WJ2QFJ1O0YBH" # replace with your ZONE ID
 DOMAIN_NAME="chaliki.site" # replace with your domain
@@ -10,7 +11,7 @@ DOMAIN_NAME="chaliki.site" # replace with your domain
 #for instance in ${INSTANCES[@]}
 for instance in $@
 do
-    INSTANCE_ID=$(aws ec2 run-instances --image-id ami-09c813fb71547fc4f --instance-type t3.micro --security-group-ids sg-07ea3d62300302f95 --tag-specifications "ResourceType=instance,Tags=[{Key=Name, Value=$instance}]" --query "Instances[0].InstanceId" --output text)
+    INSTANCE_ID=$(aws ec2 run-instances --image-id ami-09c813fb71547fc4f --instance-type t3.micro --security-group-ids $SG_ID --subnet-id $SUBNET_ID \ --tag-specifications "ResourceType=instance,Tags=[{Key=Name, Value=$instance}]" --query "Instances[0].InstanceId" --output text)
     if [ $instance != "frontend" ]
     then
         IP=$(aws ec2 describe-instances --instance-ids $INSTANCE_ID --query "Reservations[0].Instances[0].PrivateIpAddress" --output text)
