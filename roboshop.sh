7815ae404ed2ae62dd1f7593e0513f1579e51d28
Umachaliki29 <umashankari29@gmail.com>
2025-05-25 06:02:34 +0100

    added

--- roboshop.sh
+++ roboshop.sh
@@ -1,25 +1,19 @@
 #!/bin/bash
 
 AMI_ID="ami-09c813fb71547fc4f"
-SG_ID="sg-07ea3d62300302f95" # replace with your SG ID
-SUBNET_ID="subnet-02dc2ea3700e04683"
+SG_ID="sg-0004c41560075b525" # replace with your SG ID
+# SUBNET_ID="subnet-02dc2ea3700e04683"
+VPC_ID="vpc-09119d95499455ca2"
 INSTANCES=("mongodb" "catalogue" "frontend")
 ZONE_ID="Z06495662WJ2QFJ1O0YBH" # replace with your ZONE ID
 DOMAIN_NAME="chaliki.site" # replace with your domain
 
 
 #for instance in ${INSTANCES[@]}
+#for instance in ${INSTANCES[@]}
 for instance in $@
 do
-    INSTANCE_ID=$(aws ec2 run-instances \
-  --image-id $AMI_ID \
-  --instance-type t3.micro \
-  --security-group-ids $SG_ID \
-  --subnet-id $SUBNET_ID \
-  --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=$instance}]" \
-  --query "Instances[0].InstanceId" \
-  --output text)
-
+    INSTANCE_ID=$(aws ec2 run-instances --image-id ami-09c813fb71547fc4f --instance-type t3.micro --security-group-ids sg-0004c41560075b525 --tag-specifications "ResourceType=instance,Tags=[{Key=Name, Value=$instance}]" --query "Instances[0].InstanceId" --output text)
     if [ $instance != "frontend" ]
     then
         IP=$(aws ec2 describe-instances --instance-ids $INSTANCE_ID --query "Reservations[0].Instances[0].PrivateIpAddress" --output text)
@@ -29,7 +23,6 @@ do
         RECORD_NAME="$DOMAIN_NAME"
     fi
     echo "$instance IP address: $IP"
-
     aws route53 change-resource-record-sets \
     --hosted-zone-id $ZONE_ID \
     --change-batch '
