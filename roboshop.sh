13102324c786c66f67170bf827d3de96ce0e4cba
Umachaliki29 <umashankari29@gmail.com>
2025-06-01 18:09:34 +0100

    commiting functions

--- roboshop.sh
+++ roboshop.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
 AMI_ID="ami-09c813fb71547fc4f" 
-SG_ID="sg-0004c41560075b525" # replace with your SG ID sg-0004c41560075b525
-SUBNET_ID="subnet-069adc0a563ff6839"
-VPC_ID="vpc-09119d95499455ca2"
+SG_ID="sg-01911fbb543963546" # replace with your SG ID sg-0004c41560075b525
+SUBNET_ID="subnet-01b5dce78b401a44f"
+VPC_ID="vpc-0028c24f9accf76e5"
 INSTANCES=("mongodb" "catalogue" "frontend")
 ZONE_ID="Z06495662WJ2QFJ1O0YBH" # replace with your ZONE ID
 DOMAIN_NAME="chaliki.site" # replace with your domain
@@ -11,7 +11,7 @@ DOMAIN_NAME="chaliki.site" # replace with your domain
 
 for instance in $@
 do
-    INSTANCE_ID=$(aws ec2 run-instances --image-id ami-09c813fb71547fc4f --instance-type t3.micro --security-group-ids sg-0004c41560075b525 --subnet-id subnet-069adc0a563ff6839 --associate-public-ip-address --tag-specifications "ResourceType=instance,Tags=[{Key=Name, Value=$instance}]" --query "Instances[0].InstanceId" --output text)
+    INSTANCE_ID=$(aws ec2 run-instances --image-id ami-09c813fb71547fc4f --instance-type t3.micro --security-group-ids sg-01911fbb543963546--subnet-id subnet-01b5dce78b401a44f --associate-public-ip-address --tag-specifications "ResourceType=instance,Tags=[{Key=Name, Value=$instance}]" --query "Instances[0].InstanceId" --output text)
     if [ $instance != "frontend" ]
     then
         IP=$(aws ec2 describe-instances --instance-ids $INSTANCE_ID --query "Reservations[0].Instances[0].PrivateIpAddress" --output text)
