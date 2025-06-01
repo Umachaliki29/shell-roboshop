23481ec5193f61999cc3e37c8651323aad90dd3f
Umachaliki29 <umashankari29@gmail.com>
2025-06-01 18:13:36 +0100

    commiting functions

--- roboshop.sh
+++ roboshop.sh
@@ -11,7 +11,7 @@ DOMAIN_NAME="chaliki.site" # replace with your domain
 
 for instance in $@
 do
-    INSTANCE_ID=$(aws ec2 run-instances --image-id ami-09c813fb71547fc4f --instance-type t3.micro --security-group-ids sg-01911fbb543963546--subnet-id subnet-01b5dce78b401a44f --associate-public-ip-address --tag-specifications "ResourceType=instance,Tags=[{Key=Name, Value=$instance}]" --query "Instances[0].InstanceId" --output text)
+    INSTANCE_ID=$(aws ec2 run-instances --image-id ami-09c813fb71547fc4f --instance-type t3.micro --security-group-ids sg-01911fbb543963546 --vpc-id vpc-0028c24f9accf76e5 --subnet-id subnet-01b5dce78b401a44f --associate-public-ip-address --tag-specifications "ResourceType=instance,Tags=[{Key=Name, Value=$instance}]" --query "Instances[0].InstanceId" --output text)
     if [ $instance != "frontend" ]
     then
         IP=$(aws ec2 describe-instances --instance-ids $INSTANCE_ID --query "Reservations[0].Instances[0].PrivateIpAddress" --output text)
